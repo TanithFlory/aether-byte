91b4c8e5396b7ce400ed0e0e0e58375e9d7938d1
TanithFlory <tanithflory7770@gmail.com>
2023-08-08 15:18:48 +0530

    contact form functionality

--- src/app/Components/ContactForm/ContactForm.tsx
+++ src/app/Components/ContactForm/ContactForm.tsx
@@ -15,9 +15,11 @@ function ContactForm() {
     message: "",
   });
 
-  function formChangeHandler(
-    e: React.FormEvent<HTMLInputElement | HTMLSelectElement>
-  ) {
+  const formChangeHandler = (
+    e: React.FormEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => {
     const target = e.target as HTMLInputElement;
     setFormData((prev) => {
       return {
@@ -25,13 +27,21 @@ function ContactForm() {
         [target.name]: target.value,
       };
     });
-  }
+  };
+
+  const formSubmitHandler = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <form className="bg-[#1a1b3d] p-6 h-[400px] w-full max-w-[550px] flex-[1 1 100%]">
+    <form
+      className="bg-[#1a1b3d] px-6 py-12 min-h-[500px] w-full max-w-[550px] flex-[1 1 100%]"
+      onSubmit={formSubmitHandler}
+    >
       <div className="flex flex-col gap-4 h-full ">
-        <div className="flex gap-4 flex-wrap items-center justify-center">
-          <BasicDetails onChange={formChangeHandler} formData={formData}/>
-          <DropDown onChange={formChangeHandler} formData={formData}/>
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4">
+          <BasicDetails onChange={formChangeHandler} formData={formData} />
+          <DropDown onChange={formChangeHandler} formData={formData} />
         </div>
         <div className="flex flex-col h-full">
           <label className="mb-2 text-fs-100">Message</label>
@@ -41,6 +51,8 @@ function ContactForm() {
             textArea={true}
             className="h-[70%] resize-none"
             maxLength={208}
+            name="message"
+            onChange={formChangeHandler}
           />
         </div>
         <button
--- src/app/Utils/TextInput.tsx
+++ src/app/Utils/TextInput.tsx
@@ -21,7 +21,6 @@ function TextInput({
   value,
   ...props
 }: IProps) {
-  console.log(value)
   const stylings =
     "h-[38px] text-gray text-fs-200 bg-[#242527] px-2 py-4 box-border border-b-4 border-b-gray outline-0 focus:border-b-lightGreen placeholder:text-gray";
   return textArea ? (
