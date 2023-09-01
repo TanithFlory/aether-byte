d0471b9143842aa61bc9b011ce3e821ef448ce52
TanithFlory <tanithflory7770@gmail.com>
2023-09-01 15:29:38 +0530

    add office location component

--- src/app/Components/OfficeLocation/OfficeLocation.tsx
+++ src/app/Components/OfficeLocation/OfficeLocation.tsx
@@ -1,30 +1,16 @@
-"use client";
-
 import { WrapperDiv } from "@/app/Utils/WrapperDiv";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
 import InformationCards from "./InformationCards";
+import dynamic from "next/dynamic";
 function OfficeLocation() {
+  const Map = dynamic(() => import("./Map"), { ssr: false });
+
   return (
     <section className="bg-secondary py-24">
       <WrapperDiv className="gap-5">
-        <div className="w-full max-w-[450px]  text-primary bg-[#1F1D21]">
+        <div className="w-full max-w-[450px]  text-primary bg-primary rounded-sm">
           <InformationCards />
         </div>
-        <MapContainer
-          center={[41.91065, -87.64798]}
-          zoom={13}
-          scrollWheelZoom={false}
-          className="w-full max-w-[700px]"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[41.91065, -87.64798]}>
-            <Popup>Our location</Popup>
-          </Marker>
-        </MapContainer>
+        <Map/>
       </WrapperDiv>
     </section>
   );
